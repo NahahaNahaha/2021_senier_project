a1276195737a2ca2806193dd8b77862ca39cb4b1
NahahaNahaha <hawng000@naver.com>
2021-08-02 23:33:16 +0900

    environment
    강화학습 투자 - 환경 모듈 (주가 데이터 관리)

---
+++ environment.py
@@ -0,0 +1,28 @@
+class Environment:
+    PRICE_IDX = 4 # 종가의 위치
+
+    def __init__(self, chart_data = None):
+        self.chart_data = chart_data # 주식 종목의 차트 데이터
+        self.observation = None # 현재 관측치
+        self.idx = -1  # 차트 데이터에서의 현재 위치
+
+    def reset(self):
+        self.observation = None
+        self.idx = -1
+
+    def observe(self):
+        if len(self.chart_data) > self.idx + 1:
+            self.idx += 1
+            self.observation = self.chart_data.iloc[self.idx]
+            return self.observation
+        return None
+    
+    def get_price(self):
+        if self.observation != None:
+            return self.observation[self.PRICE_IDX]
+        return None
+
+    def set_chart_data(self, chart_data):
+        self.chart_data = chart_data
+
+
