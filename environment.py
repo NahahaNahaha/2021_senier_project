f8ad0baf904088ff47a8672bbfb800c30769ffa4
NahahaNahaha <hawng000@naver.com>
2021-08-07 23:51:37 +0900

    environment
    add description and change value

--- environment.py
+++ environment.py
@@ -6,18 +6,19 @@ class Environment:
         self.observation = None # 현재 관측치
         self.idx = -1  # 차트 데이터에서의 현재 위치
 
-    def reset(self):
+    def reset(self): # 리셋 , 학습을 위해
         self.observation = None
         self.idx = -1
 
-    def observe(self):
+    def observe(self): #관측 -> 뒤에 자세히 나옴
+
         if len(self.chart_data) > self.idx + 1:
             self.idx += 1
             self.observation = self.chart_data.iloc[self.idx]
             return self.observation
         return None
     
-    def get_price(self):
+    def get_price(self): # 현재 관측에서 종가 얻기
         if self.observation != None:
             return self.observation[self.PRICE_IDX]
         return None
